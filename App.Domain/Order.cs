bc75acb50e40cc28b0bd6f3cde2fd0c791f125f6
avocadoseedd <sevcancicek355@gmail.com>
2024-05-27 15:06:52 +0300

    Order class

---
+++ App.Domain/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain
+{
+     public class Order
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public string OrderNumber {  get; set; }
+
+        public string OrderDate { get; set; }
+        public string OrderType { get; set; }
+    }
+}
